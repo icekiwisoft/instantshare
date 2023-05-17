757f2dd1ea5651400dc923dd392d52010b1e14d3
ngdream <ngdream1953@gmail.com>
2023-05-17 21:05:33 +0100

    new

--- src/app/share/share.page.ts
+++ src/app/share/share.page.ts
@@ -1,17 +1,53 @@
 import { Component } from '@angular/core';
 import { ActionSheetController, IonicModule } from '@ionic/angular';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { BleClient, ScanResult, numberToUUID } from '@capacitor-community/bluetooth-le';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'share.page.html',
   styleUrls: ['share.page.scss'],
   standalone: true,
-  imports: [IonicModule, ExploreContainerComponent]
+  imports: [IonicModule, ExploreContainerComponent,CommonModule]
 })
+
 export class SharePage {
 
   constructor(public actionSheetController: ActionSheetController) { }
+
+
+  bluetoothScanResults: ScanResult[] = [];
+  readonly HEART_RATE_SERVICE = numberToUUID(0x180d);
+
+  onBluetoothDeviceFound(result:any) {
+    console.log('received new scan result', result);
+    this.bluetoothScanResults.push(result);
+  }
+
+async scan(): Promise<void> {
+  try {
+    await BleClient.initialize();
+
+    await BleClient.requestLEScan(
+      {
+        services: [this.HEART_RATE_SERVICE],
+
+      },
+      (result) => {
+        this.onBluetoothDeviceFound.bind(result)
+      }
+    );
+
+    setTimeout(async () => {
+      await BleClient.stopLEScan();
+      console.log('stopped scanning');
+    }, 5000);
+  } catch (error) {
+    console.error(error);
+  }
+}
 
   async presentActionSheet() {
     const actionSheet = await this.actionSheetController.create({
@@ -38,4 +74,8 @@ export class SharePage {
     const { role } = await actionSheet.onDidDismiss();
     console.log('onDidDismiss resolved with role', role);
   }
+
+
+
+
 }
